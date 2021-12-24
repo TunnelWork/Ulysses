49613e2bf1bb128a2a09ce3131e72a7b7fe9a6c8
Gaukas Wang <i@gaukas.wang>
2021-12-24 16:30:49 +0800

    Staging: logging details in error handling
    - logging funcName fetched by runtime package.

--- src/internal/utils/error_handling.go
+++ src/internal/utils/error_handling.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"net/http"
+	"runtime"
 
 	harpocrates "github.com/TunnelWork/Harpocrates"
 	"github.com/TunnelWork/Ulysses.Lib/api"
@@ -13,13 +14,19 @@ func HandleError(c *gin.Context, err error) {
 	if err == nil {
 		return // won't handle nil
 	}
+
+	// Get information about where the error occured
+	// Credits: https://stackoverflow.com/a/53012754/10469909
+	pc, _, _, _ := runtime.Caller(1)
+	funcName := runtime.FuncForPC(pc).Name()
+
 	// Generate error random index for logging
 	index, err := harpocrates.GetRandomBase32()
 	if err != nil {
 		index = "N/A"
 	}
 	// Log error
-	logging.Error("[%s] %s", index, err.Error())
+	logging.Error("[%s] %s: %s", index, funcName, err.Error())
 
 	c.AbortWithStatusJSON(http.StatusInternalServerError, api.PayloadResponse(api.ERROR, gin.H{
 		"ref": index,
